d593bcec7016373d99ebee821b7ba9da47558613
sp-ho <106715796+sp-ho@users.noreply.github.com>
2024-12-09 20:25:41 -0500

    Update AlreadyExistsException.java

--- src/main/java/com/prog2/main/AlreadyExistsException.java
+++ src/main/java/com/prog2/main/AlreadyExistsException.java
@@ -1,9 +1,7 @@
 package com.prog2.main;
 
 public class AlreadyExistsException extends Exception{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	public AlreadyExistsException(String warning) {
